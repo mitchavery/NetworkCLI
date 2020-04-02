e75c3ee656aaeb377994394d203f5c1a57ba89d5
mitchavery <avery.mitch651@gmail.com>
2020-04-01 22:43:51 -0700

    error handling

--- cmd/my-cli/cli.go
+++ cmd/my-cli/cli.go
@@ -13,9 +13,6 @@ func main() {
     app := cli.NewApp()
     app.Name = "Website Lookup CLI"
     app.Usage = "Let's you query IPs, CNAMEs, MX records and Name Servers!"
-
-    // We'll be using the same flag for all our commands
-    // so we'll define it up here
     myFlags := []cli.Flag{
         cli.StringFlag {
             Name:  "host",
@@ -29,16 +26,14 @@ func main() {
             Name:  "ns",
             Usage: "Looks Up the NameServers for a Particular Host",
             Flags: myFlags,
-            // the action, or code that will be executed when
-            // we execute our `ns` command
+
             Action: func(c *cli.Context) error {
                 // a simple lookup function
                 ns, err := net.LookupNS(c.String("url"))
                 if err != nil {
                     return err
                 }
-                // we log the results to our console
-                // using a trusty fmt.Println statement
+
                 for i := 0; i < len(ns); i++ {
                     fmt.Println(ns[i].Host)
                 }
@@ -47,7 +42,6 @@ func main() {
         },
     }
 
-    // start our application
     err := app.Run(os.Args)
     if err != nil {
         log.Fatal(err)
